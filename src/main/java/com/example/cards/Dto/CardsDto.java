5eb7219ecd6c8ffa5111bfab54245fe69eaff63e
JayPande13 <jmaypande@gmail.com>
2024-05-28 16:51:02 +0530

    final commit of cards

--- src/main/java/com/example/cards/Dto/CardsDto.java
+++ src/main/java/com/example/cards/Dto/CardsDto.java
@@ -1,6 +1,8 @@
 package com.example.cards.Dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -8,20 +10,26 @@ import lombok.Data;
 public class CardsDto {
 
     @Schema(description = "Mobile Number Linked with the card",example = "7351733655")
+    @Pattern(regexp = "^$|[0-9]{10}",message = "Mobile Number must be 10 digits")
     public String mobileNumber;
 
     @Schema(description = "Cards Number",example = "5548 5544 5548 7584")
+    @NotEmpty(message = "Card Number Cannot be Empty")
     public String cardNumber;
 
     @Schema(description = "Type Of card",example = "Platinium, Rubyx etc")
+    @NotEmpty(message = "Card Type Cannot be Empty")
     public String cardType;
 
     @Schema(description = "Amount used up from Card" , example = "65485")
+    @NotEmpty(message = "Amount used Cannot be Empty")
     public int amountUsed;
 
     @Schema(description = "Total Limit in Card" , example = "80000")
+    @NotEmpty(message = "Total Limit Cannot be Empty")
     public int totalLimit;
 
     @Schema(description = "Available Balance in Card" , example = "14515")
+    @NotEmpty(message = "Available Amount Cannot be Empty")
     public int availableAmount;
 }
--- src/main/java/com/example/cards/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/cards/Exception/GlobalExceptionHandler.java
@@ -1,16 +1,37 @@
 package com.example.cards.Exception;
 
 import com.example.cards.Dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        HashMap<String,String> validationErrorMap = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+        validationErrorList.forEach(error -> {
+            String errorMessage = error.getDefaultMessage();
+            String errorFieldName = ((FieldError) error).getField();
+            validationErrorMap.put(errorFieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(validationErrorMap,HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> resourceMessageNotFoundHandler(ResourceNotFoundException exception, WebRequest webRequest) {
